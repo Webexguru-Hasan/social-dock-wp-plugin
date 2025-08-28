0d6e530f58069fed612080ec4b39e34525b1a4ea
Hasan wazid <webexguruhasan95@gmail.com>
2025-08-28 13:06:14 +0600

    Social dock wp plugin initial setup finished. added plugin header required content

---
+++ social-dock-wp-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:         Social Dock Wp Plugin
+ * Plugin URI:          https://wordpress.org/plugins/social-dock-wp-plugin/
+ * Description:         Social Link Integration Plugin For WordPress.
+ * Requires at least:   5.2
+ * Requires PHP:        7.4
+ * Author:              Orphior Agency
+ * Author URI:          https://hasan-wazid-portfolio.vercel.app/
+ * License:             GPL-2.0+
+ * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:          https://github.com/Webexguru-Hasan
+ * Text Domain:         social-dock-wp-plugin
+ * Domain Path:         /languages
+ */
